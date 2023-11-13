69d8ca2008c33bb78f85290d6402412f82e23963
Jongok Yoon <yoonjongok1209@gmail.com>
2023-11-13 21:44:42 +0100

    feat: 🎸 Setup pallete in tailwind config

---
+++ src/styles/theme.ts
@@ -0,0 +1,60 @@
+type Screen = 'mobile' | 'tablet' | 'laptop' | 'desktop';
+export const breakPoints: Record<Screen, string> = {
+  mobile: '375px',
+  tablet: '768px',
+  laptop: '1280px',
+  desktop: '1440px',
+};
+
+const theme = {
+  colors: {
+    white: '#FFFFFF',
+    black: '#000',
+    purple: {
+      primary: '#635FC7',
+      secondary: '#A8A4FF',
+    },
+    red: {
+      primary: '#EA5555',
+      secondary: '#FF9898',
+    },
+    grey: {
+      100: '#F4F7FD',
+      200: '#E4EBFA',
+      300: '#828FA3',
+      400: '#3E3F4E',
+      500: '#2B2C37',
+      600: '#20212C',
+      700: '#20212C',
+      800: '#000112',
+    },
+  },
+
+  width: breakPoints,
+  minWidth: breakPoints,
+  maxWidth: breakPoints,
+  minHeight: breakPoints,
+  maxHeight: breakPoints,
+  screens: breakPoints,
+  borderRadius: {
+    lg: 'var(--radius)',
+    md: 'calc(var(--radius) - 2px)',
+    sm: 'calc(var(--radius) - 4px)',
+  },
+  keyframes: {
+    'accordion-down': {
+      from: { height: 0 },
+      to: { height: 'var(--radix-accordion-content-height)' },
+    },
+    'accordion-up': {
+      from: { height: 'var(--radix-accordion-content-height)' },
+      to: { height: 0 },
+    },
+  },
+  animation: {
+    'accordion-down': 'accordion-down 0.2s ease-out',
+    'accordion-up': 'accordion-up 0.2s ease-out',
+  },
+} as const;
+
+export default theme;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,24 @@
-import type { Config } from 'tailwindcss';
+import theme from '@/styles/theme';
 
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: theme.colors,
+    screens: theme.screens,
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
       },
     },
+    extend: theme,
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 };
-export default config;
